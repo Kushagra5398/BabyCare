c922c8555fac407078520c8f36d02a0f66d70bd9
Kushagra Goel <66068156+Kushagra5398@users.noreply.github.com>
2020-08-01 20:36:40 +0530

    Initial Commit

---
+++ src/com/servlets/DeleteBabyName.java
@@ -0,0 +1,25 @@
+package com.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.dao.BabyDao;
+@WebServlet("/DeleteBabyName")
+public class DeleteBabyName extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String sid=request.getParameter("id");
+		int id=Integer.parseInt(sid);
+		BabyDao.delete(id);
+		
+		
+		response.sendRedirect("ViewBabyName");
+		
+	}
+
+
+}
